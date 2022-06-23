4affd903d295d2f56168693d6bbf4688f017cd2f
Raninder Dhillon <raninderdhillon@gmail.com>
2022-06-23 09:45:52 -0400

    Create multipleif.js

---
+++ javascript/multipleif.js
@@ -0,0 +1,19 @@
+let num1=20, num2=10;
+let choice=2, result;
+if(choice==1){
+  result=num1+num2;
+}
+else if(choice===2){
+	result= num1-num2;
+}
+else if(choice==3){
+result= num1*num2;
+}
+else if(choice==4){
+	result= num1/num2;
+	}
+	else{
+		result=0;
+	}
+	console.log("result",result);
+	// console.log(`result${result}`);
