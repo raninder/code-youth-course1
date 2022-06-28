90bf86103ed382ce87938a9384b936fc9a54900d
Raninder Dhillon <raninderdhillon@gmail.com>
2022-06-28 11:02:04 -0400

    Create array1.js

---
+++ javascript/array1.js
@@ -0,0 +1,21 @@
+// let marks1=20;
+// let marks2=30;
+// let marks3=12;
+// let marks4=23;
+// marks1+marks2+marks3+mark4
+
+let marks = [20,30,12,23];
+
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+// for(let i=0; i<marks.length;i++){
+// 	console.log(marks[i]);
+// }
+
+let names = ['apples','banana','mango'];
+console.log("array size",names.length);
+
+
+// take different arrays that store different values
+// String numbers  numbers, boolean
