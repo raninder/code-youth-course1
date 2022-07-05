81c95723155b9f8e1e7c07ef57074d25e6d8a9db
Raninder Dhillon <raninderdhillon@gmail.com>
2022-07-05 11:21:28 -0400

    let vs var

---
+++ javascript/vars.js
@@ -0,0 +1,20 @@
+// var a=5;
+// {
+// 	var a=3;
+// }
+// console.log(a);
+// let b=5;
+// // let b=7;
+// {
+// 	let b=3;
+// }
+// console.log(b);
+// hoisting
+a=5;
+console.log(a);
+//let a;   //show error
+var a;
+
+let c=222;
+c=c+4;
+console.log(c);
